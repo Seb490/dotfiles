d26c49edc3328f0ce7496466072224b8edb42228
swe490 <swe490@users.noreply.github.com>
2021-04-05 00:34:41 +0200

    Edit bootstrap script
    - disable linuxbrew install
    - disable sublime text install

--- bootstrap_apps_linux.sh
+++ bootstrap_apps_linux.sh
@@ -7,21 +7,21 @@ sudo apt-get install git vim zsh curl htop sox build-essential
 sudo update-alternatives --set editor /usr/bin/vim.basic
 
 # Install linuxbrew
-sh -c "$(curl -fsSL  https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+#sh -c "$(curl -fsSL  https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 
 # Install Sublimetext 
 # Install the GPG key:
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+#wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 
 # Ensure apt is set up to use https sources
-sudo apt-get install apt-transport-https
+#sudo apt-get install apt-transport-https
 
 # Select the stable channel
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+#echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 # Update apt sources and install Sublime Text 
-sudo apt-get update
-sudo apt-get install sublime-text
+#sudo apt-get update
+#sudo apt-get install sublime-text
 
 # Install VS Code
 #sudo snap install vscode --classic
