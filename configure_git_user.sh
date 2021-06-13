488260b3710ba9db8227bde0fe8a30bb5d246fba
Seb490 <Seb490@users.noreply.github.com>
2021-06-13 18:53:34 +0200

    change github user name to Seb490

--- configure_git_user.sh
+++ configure_git_user.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-git config --local  user.email "swe490@users.noreply.github.com"
-git config --local  user.name "swe490" 
+git config --local  user.email "Seb490@users.noreply.github.com"
+git config --local  user.name "Seb490" 
 
